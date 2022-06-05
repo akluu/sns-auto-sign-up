37fbe9b6a833f012c67f1988cf6ff464b0d0948d
akluu <81354105+akluu@users.noreply.github.com>
2022-06-05 10:54:06 -0700

    initial commit

---
+++ updateTable.py
@@ -0,0 +1,51 @@
+import boto3
+import json
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('userId')
+
+def create_user(id):
+    item = {"userId": id, "phoneNumber": "", "subscriptionARN": ""}
+    result = table.put_item(Item = item)
+    status_code = result['ResponseMetadata']['HTTPStatusCode']
+    return getResponse(status_code, json.dumps(item))
+    
+def delete_user(id):
+    key = {"userId": id}
+    result = table.delete_item(Key = key)
+    status_code = result['ResponseMetadata']['HTTPStatusCode']
+    return getResponse(status_code, json.dumps({"status": id + " deleted"}))
+    
+def get_user(id):
+    key = {"userId": id}
+    result = table.get_item(Key = key)
+    status_code = result['ResponseMetadata']['HTTPStatusCode']
+    return getResponse(status_code, result["Item"])
+    
+def add_phone_number(id, number, subscriptionARN):
+    key = {"userId": id}
+    result = table.update_item(Key = key, 
+    UpdateExpression = "Set phoneNumber=:p, subscriptionARN=:s ",
+    ExpressionAttributeValues = {
+        ':p' : number,
+        ':s' : subscriptionARN
+    },
+    ReturnValues = "UPDATED_NEW")
+    status_code = result['ResponseMetadata']['HTTPStatusCode']
+    return getResponse(status_code, result['Attributes'])
+    
+def getResponse(status_code, body):
+    if status_code == 200:
+        response = {
+            "statusCode": status_code,
+            "headers": {"Access-Control-Allow-Origin": "*"},
+            "body": body
+        }
+        return response
+    else:
+        return {
+            "statusCode": status_code,
+            "headers": {"Access-Control-Allow-Origin": "*"},
+            "body": json.dumps({"status": "request invalid"})
+        }
+
